fc1ecfb8d306037a3f79e4161be016d6c7f6674e
Hopper262 <hopper@whpress.com>
2022-08-13 19:30:49 -0400

    Support UTF-16 encoded FStrings

--- UnrealEngine.Gvas/BinaryReaderEx.cs
+++ UnrealEngine.Gvas/BinaryReaderEx.cs
@@ -8,9 +8,11 @@ public static class BinaryReaderEx
     public static string? ReadFString(this BinaryReader reader)
     {
         int length = reader.ReadInt32();
-        if (length is > 512 or < 0)
+        if (length is > 512 or < -512)
             throw new SaveGameException($"Sanity check failed: FString length is {length}");
 
+        if (length is < 0)
+            return Encoding.Unicode.GetString(reader.ReadBytes(length * -2)).TrimEnd('\0');
         return Encoding.ASCII.GetString(reader.ReadBytes(length)).TrimEnd('\0');
     }
 
@@ -18,9 +20,20 @@ public static class BinaryReaderEx
     {
         if (str is {Length: > 0})
         {
-            writer.Write(str!.Length + 1);
-            writer.Write(str.ToCharArray());
-            writer.Write((byte) 0);
+            if (Encoding.UTF8.GetByteCount(str) == str.Length)
+            {
+                // safe to use 7-bit ASCII encoding
+                writer.Write(str!.Length + 1);
+                writer.Write(str.ToCharArray());
+                writer.Write((byte) 0);
+            }
+            else
+            {
+                // use UTF-16 LE encoding
+                writer.Write(-1 - Encoding.Unicode.GetByteCount(str)/2);
+                writer.Write(Encoding.Unicode.GetBytes(str));
+                writer.Write((short) 0);
+            }
         }
         else
         {
